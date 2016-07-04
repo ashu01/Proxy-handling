d7ccc788fb6ee229e714572f048bb96ecc8741f9
ashu01 <as01hu@gmail.com>
2016-07-04 14:55:43 +0530

    find port for specific type(tcp http)

---
+++ examples/LookupPort/lookupServiceport.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr,
+			"\nUsage: %s network-type service\n",
+			os.Args[0])
+		os.Exit(1)
+	}
+
+	networkType := os.Args[1]
+	service := os.Args[2]
+
+	port, err := net.LookupPort(networkType, service)
+
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+		os.Exit(2)
+	}
+
+	fmt.Println("Service port ", port)
+	os.Exit(0)
+}
+
+// go run lookupServiceport.go tcp ftp
+// Service port  21
